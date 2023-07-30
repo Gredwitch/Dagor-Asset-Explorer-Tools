780d4cf32b797fa52b1e3fa2cd0fb2730a243976
Quentin <32811753+Gredwitch@users.noreply.github.com>
2023-07-30 19:03:12 +0200

    Updated version number to 1.1.1

--- io_scene_daet/__init__.py
+++ io_scene_daet/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
 	"name": "Dagor Asset Explorer Tools",
 	"author": "Quentin H.",
-	"version": (1, 1, 0),
+	"version": (1, 1, 1),
 	"blender": (2, 92, 0),
 	"category": "Import",
 	"location": "File > Import",

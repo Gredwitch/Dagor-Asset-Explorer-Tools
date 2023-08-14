b3505b37380bcbe949ba65f55148b8d72548ad30
Gredwitch <32811753+Gredwitch@users.noreply.github.com>
2023-08-14 02:10:14 +0200

    Increased version number

--- io_scene_daet/__init__.py
+++ io_scene_daet/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
 	"name": "Dagor Asset Explorer Tools",
 	"author": "Quentin H.",
-	"version": (1, 1, 4),
+	"version": (1, 1, 5),
 	"blender": (2, 92, 0),
 	"category": "Import",
 	"location": "File > Import",
